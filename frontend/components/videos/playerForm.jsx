6933fd9823a348842301d4baa01492fb0a53642e
patrickbrown <patricksbrown1992@gmail.com>
2019-10-31 17:27:34 -0400

    hiding arrow

--- frontend/components/videos/playerForm.jsx
+++ frontend/components/videos/playerForm.jsx
@@ -9,20 +9,49 @@ class PlayerForm extends React.Component {
             time: '00:00',
             playing: true,
         };
-        // this.goBack = this.goBack.bind(this);
+
+
+       
     }
 
-    // goBack(e){
-    //     e.preventDefault();
-    //     return <Redirect to='/videos' />
-    // }
+    componentDidMount(){
+        // let controlsToggleTimer;
+        const backToBrowse = document.getElementById("back-to-browse");
+        const backButton = document.getElementById("back-arrow-font");
+
+
+
+
+
+        const showBack = () => {
+            backButton.style.opacity = "1.0";
+            backToBrowse.style.opacity = '1.0'
+        }
+
+        const hideBack = () => {
+            backButton.style.opacity = "0.0";
+            backToBrowse.style.opacity = '0.0'
+        }
+
+        backToBrowse.addEventListener("mouseover", showBack)
+        backToBrowse.addEventListener("mouseout", hideBack)
+    }
+
 
 
     render() {
        
         return (
             <div className='video-player'>
-                <Link to='/videos'><i className="fas fa-arrow-left fa-2x"></i></Link> 
+                <Link to={"/videos"}>
+                    <div id="back-to-browse" className="back-to-browse">
+                        <i className="fas fa-arrow-left fa-2x" id="back-arrow-icon"></i>
+                        <h1 id="back-arrow-font" className="back-arrow-font">Back to Browse</h1>
+                    </div>
+                </Link>
+                
+                <br/>
+                <br/>
                 <h1>Hi</h1>
             </div>
 
