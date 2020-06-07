ee6b5016e1fd67bda1be8d7826df69f8fa42dabb
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-06-07 05:30:58 -0400

    list actions

---
+++ frontend/actions/listActions.jsx
@@ -0,0 +1,66 @@
+import * as APIUtil from '../util/listUtil';
+
+export const RECEIVE_LIST = 'RECEIVE_LIST';
+export const RECEIVE_LISTS = "RECEIVE_LISTS";
+export const DESTROY_LIST = 'DESTROY_LIST';
+export const CLEAR_LISTS = 'CLEAR_LISTS';
+
+
+
+
+const receiveList = list => {
+    return ({
+        type: RECEIVE_LIST,
+        list
+    });
+};
+
+const destroyList = list => {
+
+    return ({
+        type: DESTROY_LIST,
+        list
+    });
+};
+
+const receiveLists = lists => {
+    
+    return ({
+
+        type: RECEIVE_LISTS,
+        lists
+    });
+};
+
+export const clearLists = () => {
+    return {
+        type: CLEAR_LISTS
+    }
+}
+
+export const getLists = (user) => dispatch => (
+    APIUtil.getLists(user).then(notebooks => (dispatch(receiveLists(notebooks))))
+);
+
+export const getList = (id) => dispatch => (
+    APIUtil.getList(id).then(list => (dispatch(receiveList(list))))
+);
+
+export const createList = (list) => dispatch => {
+
+    return APIUtil.createList(list).then(list => dispatch(receiveList(list)));
+
+};
+
+export const updateList = (list) => dispatch => (
+    APIUtil.updateList(list).then(list => (dispatch(receiveList(list))))
+);
+
+export const deleteList = (list) => dispatch => {
+    
+    return APIUtil.deleteList(list).then(list => (dispatch(destroyList(list))));
+};
+
+
+
+    
