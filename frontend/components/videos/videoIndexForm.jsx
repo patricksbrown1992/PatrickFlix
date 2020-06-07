5ec18f7120f7c0d45aea155990107fa9b0e1fba4
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-06-06 23:13:27 -0400

    working logout modal. Needs stylizing

--- frontend/components/videos/videoIndexForm.jsx
+++ frontend/components/videos/videoIndexForm.jsx
@@ -10,6 +10,7 @@ class VideoIndexForm extends React.Component {
         this.state = { search: false, test: 1, loaded: null };
         this.handleSubmitLogOut = this.handleSubmitLogOut.bind(this);
         this.searchClick = this.searchClick.bind(this);
+        this.showLogOut = this.showLogOut.bind(this);
      
 
     }
@@ -23,7 +24,25 @@ class VideoIndexForm extends React.Component {
     componentDidMount(){
        
         this.props.getVideos().then(() => this.setState({loaded: true}))
-    
+        
+
+
+       
+
+    }
+
+    showLogOut(){
+        const modal = document.getElementById("log-out-modal");
+        // debugger
+        if(modal.style.opacity == "1"){
+            modal.style.opacity = "0"
+            modal.style.zIndex = 1;
+        } else {
+            modal.style.opacity = "1.0"
+            modal.style.zIndex = 5;
+        }
+        
+        
 
     }
 
@@ -55,22 +74,24 @@ class VideoIndexForm extends React.Component {
             return null;
         }
         let vids = this.props.videos;
-        let stewart = vids.slice(0, 3);
+        let stewart = vids.slice(0, 7);
         // .map((vid, index)=> (
            
         //     <IndexItem  video={vid} index={index}/>
         // ))
-        let swayze = vids.slice(3, 7);
+        let swayze = vids.slice(7, 13);
         // .map((vid, index) => (
            
         //     <IndexItem  video={vid} index={index+3}/>
         // ))
-        let harris = vids.slice(7, 10);
+        let harris = vids.slice(13, 19);
         // .map((vid, index) => (
-        let wilson = vids.slice(10,14)
-        let dempsey = vids.slice(14)
+        let wilson = vids.slice(19,25)
+        let dempsey = vids.slice(25)
         //     <IndexItem  video={vid} index={index+7}/>
         // ))
+        
+        
         
         return (
             <div className='videos-index'>
@@ -92,8 +113,10 @@ class VideoIndexForm extends React.Component {
 
 
                         <img src={window.icon} />
-                        <button className = 'log-out-button' onClick={this.handleSubmitLogOut}><i  className="fas fa-caret-down fa-2x"></i></button>
-
+                        <button onClick={this.showLogOut} id = 'log-out-arrow' ><i  className="fas fa-caret-down fa-2x"></i></button>
+                        <div id='log-out-modal'>
+                            <button id = 'log-out-button' onClick={this.handleSubmitLogOut}>Log Out</button>
+                        </div>
                     </div>
                 </div>
 
@@ -105,34 +128,34 @@ class VideoIndexForm extends React.Component {
                     <div className= 'video-row'>
                         <h1 className='featured-video-h1'>Patrick Stewart</h1>
                         
-                        <DetailRow identifier= {0} vids = {stewart} range={[0,1, 2]}/>
+                        <DetailRow identifier= {0} vids = {stewart} range={[0,1,2,3, 4, 5, 6]}/>
 
                     </div>
                     
                     <div className= 'video-row'>
                         <h1 className='featured-video-h1'>Patrick Swayze</h1>
                         
-                        <DetailRow identifier= {1} vids={swayze} range = {[3,4, 5, 6]}/>
+                        <DetailRow identifier= {1} vids={swayze} range = {[7, 8, 9, 10, 11, 12]}/>
                    </div>
                     
                     <div className= 'video-row'>
                         <h1 className='featured-video-h1'>Neil Patrick Harris</h1>
                        
-                        <DetailRow identifier= {2} vids = {harris} range={[7,8,9]}/>
+                        <DetailRow identifier= {2} vids = {harris} range={[13,14, 15, 16, 17, 18]}/>
 
                     </div>
 
                     <div className= 'video-row'>
                         <h1 className='featured-video-h1'>Patrick Wilson</h1>
                         
-                        <DetailRow identifier= {3} vids = {wilson} range={[10, 11, 12, 13]}/>
+                        <DetailRow identifier= {3} vids = {wilson} range={[19, 20, 21, 22, 23, 24]}/>
 
                     </div>
 
                     <div className= 'video-row'>
                         <h1 className='featured-video-h1'>Patrick Dempsey</h1>
                         
-                        <DetailRow identifier= {4} vids = {dempsey} range={[14, 15, 16, 17]}/>
+                        <DetailRow identifier= {4} vids = {dempsey} range={[25, 26, 27, 28, 29, 30, 31]}/>
 
                     </div>
                    
