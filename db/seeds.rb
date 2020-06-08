7789bcadaf8ff26bc90e27c8b473f7fecb5cfb4f
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-06-08 13:32:23 -0400

    fixed endings for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ ted.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/ted_traile
 ted.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/ted_thumbnail.jpg'), filename: 'ted_thumbnail.jpg')
 
 xmen = Video.create({title: "X-Men", year: 2000, runtime: "2m 05s", description: "The film depicts a world where a small proportion of people are mutants, whose possession of superhuman powers makes them distrusted by normal humans."} )
-xmen.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/X-Men_Trailer.mp4'), filename: 'X-Men_Trailer.mp4')
-xmen.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/X-Men_Thumbnail.jpeg'), filename: 'X-Men_Thumbnail.jpeg')
+gxmen.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/X-Men_Trailer'), filename: 'X-Men_Trailer')
+xmen.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/X-Men_Thumbnail'), filename: 'X-Men_Thumbnail')
 
 startrek = Video.create({title: "Star Trek: First Contact", year: 1996, runtime: "2m 19s", description: "The crew of the USS Enterprise-E travel back in time from the 24th century to the mid-21st century in order to stop the cybernetic Borg from conquering Earth by changing their past."} )
 startrek.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Star_Trek_First_Contact_Trailer.mp4'), filename: 'Star_Trek_First_Contact_Trailer.mp4')
@@ -64,12 +64,12 @@ reddawn.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/red_da
 reddawn.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/red_dawn_poster.jpg'), filename: 'red_dawn_poster.jpg')
 
 gonegirl = Video.create({title: "Gone Girl", year: 2014, runtime: "2m 42s", description: "Set in Missouri, the story begins as a mystery that follows the events surrounding Nick Dunne (Affleck), who becomes the prime suspect in the sudden disappearance of his wife Amy (Pike)."} )
-gonegirl.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Gone_Girl_Trailer.mp4'), filename: 'Gone_Girl_Trailer.mp4')
-gonegirl.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Gone_Girl_Thumbnail.jpeg'), filename: 'Gone_Girl_Thumbnail.jpeg')
+gonegirl.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Gone_Girl_Trailer'), filename: 'Gone_Girl_Trailer')
+gonegirl.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Gone_Girl_Thumbnail'), filename: 'Gone_Girl_Thumbnail')
 
 doogiehowser = Video.create({title: "Doogie Howser", year: 1989, runtime: "1m 01s", description: "The show stars Neil Patrick Harris in the title role as a teenage physician who balances the challenge of practicing medicine with the everyday problems of teenage life."} )
 doogiehowser.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/Doogie_Howser_Trailer.mp4'), filename: 'Doogie_Howser_Trailer.mp4')
-doogiehowser.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/doogie_howser_thumbnail.jpeg'), filename: 'doogie_howser_thumbnail.jpeg')
+doogiehowser.thumbnail.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/doogie_howser_thumbnail'), filename: 'doogie_howser_thumbnail')
 
 himym = Video.create({title: "How I Met Your Mother", year: 2005, runtime: "0m 46s", description: "Ted, in the year 2030, recounts to his son and daughter the events that led him to meet their mother."} )
 himym.movie.attach(io: open('https://patrickflix-seeds.s3.amazonaws.com/HIMYM_Trailer.mp4'), filename: 'HIMYM_Trailer.mp4')
