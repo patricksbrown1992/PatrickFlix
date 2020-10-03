c1fe65df28dbcd6de17e0733d47c3d0247e37be1
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 18:17:15 -0400

    convert sign up form to hook

--- frontend/components/signUp/signUpForm.jsx
+++ frontend/components/signUp/signUpForm.jsx
@@ -1,68 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class SignUpForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { email: '', password: '' }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+
+const SignUpForm = props => {
+    const [email, updateEmail] = useState(() => {
+        return '';
+    })
+    
+    const [password, updatePassword] = useState(() => {
+        return '';
+    })
+
+
+    function handleChangeEmail(e) {
+        e.preventDefault()
+        updateEmail(e.target.value)
     }
 
-    handleChange(field) {
-
-        return (e) => {
-            this.setState({ [field]: e.target.value })
-        }
+    function handleChangePassword(e) {
+        e.preventDefault()
+        updatePassword(e.target.value)
     }
 
-    handleSubmit(e) {
+
+
+    function handleSubmit(e) {
         e.preventDefault();
-        this.props.signup(this.state);
-        this.setState({ email: '', password: '' });
-        this.props.clearErrors();
+        props.signup({email, password});
+        updatePassword('')
+        updateEmail('')
+        setState({ email: '', password: '' });
+        props.clearErrors();
 
     }
 
-    render() {
-        let errors
-        if (this.props.errors.length > 1) {
-            errors = this.props.errors.map((error, idx) => {
-                return <li key={idx}>{error}</li>
-            })
-        }
-        return (
-            <div className='sign-up-form'>
-                <div className='sign-up-title'>
-                    <Link to='/'><img src={window.big} /></Link>
+  
+    
+    return (
+        <div className='sign-up-form'>
+            <div className='sign-up-title'>
+                <Link to='/'><img src={window.big} /></Link>
 
-                </div>
-                <div className='sign-in-body'>
-                    <h1>Sign Up</h1>
-                    <input type="text" value={this.state.email} placeholder='email' onChange={this.handleChange('email')} />
-                    <input type="password" value={this.state.password} placeholder='password' onChange={this.handleChange('password')} />
-                    <ul>
-                        <br />
-                        {errors}
-                        <br />
-                    </ul>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className='sign-up-continue'>
-
-                            <button type='submit'>Sign Up</button>
-                        </div>
-                    </form>
-
+            </div>
+            <div className='sign-in-body'>
+                <h1>Sign Up</h1>
+                <input type="text" value={email} placeholder='email' onChange={handleChangeEmail} />
+                <input type="password" value={password} placeholder='password' onChange={handleChangePassword} />
+                <ul>
                     <br />
+                    {props.errors ? props.errors.map((error, idx) => {
+                        return <li key={idx}>{error}</li>
+                    }) : ''}
+                    <br />
+                </ul>
+                <form onSubmit={handleSubmit}>
+                    <div className='sign-up-continue'>
 
-                    <div className='redirect-to-sign-in'>
-                        <h3>Already have an account?</h3>
-                        <Link to='/login'> Log in</Link>
+                        <button type='submit'>Sign Up</button>
                     </div>
+                </form>
+
+                <br />
+
+                <div className='redirect-to-sign-in'>
+                    <h3>Already have an account?</h3>
+                    <Link to='/login'> Log in</Link>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+    
 
 }
 
