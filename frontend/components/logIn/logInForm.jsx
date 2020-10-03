7ff3f31b474ed07dfce37a9c875e80d2d3283c6a
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-03 18:19:29 -0400

    converted log in form to hook

--- frontend/components/logIn/logInForm.jsx
+++ frontend/components/logIn/logInForm.jsx
@@ -1,66 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class LogInForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { email: '', password: '' }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+const LogInForm = props => {
+
+    const [email, updateEmail] = useState(() => {
+        return '';
+    })
+    
+    const [password, updatePassword] = useState(() => {
+        return '';
+    })
+
+
+    function handleChangeEmail(e) {
+        e.preventDefault()
+        updateEmail(e.target.value)
     }
 
-    handleChange(field) {
-
-        return (e) => {
-            this.setState({ [field]: e.target.value })
-        }
+    function handleChangePassword(e) {
+        e.preventDefault()
+        updatePassword(e.target.value)
     }
 
-    handleSubmit(e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        this.props.login(this.state);
-        this.setState({ email: '', password: '' });
-        this.props.clearErrors();
+        props.login({email, password});
+        updatePassword('')
+        updateEmail('')
+        props.clearErrors();
     }
 
-    render() {
-        let errors
-        if (this.props.errors.length > 0) {
-            errors = this.props.errors.map((error, idx) => {
-                return <li key={idx}>{error}</li>
-            })
-        }
+ 
+    
 
-        return (
-            <div className='sign-in-form'>
-                <div className='sign-in-title'>
-                    <Link to='/'><img src={window.big} /></Link>
-                </div>
-                <div className='sign-in-body'>
-                    <h1>Sign In</h1>
-                    <input type="text" value={this.state.email} placeholder='email' onChange={this.handleChange('email')} />
-                    <input type="password" value={this.state.password} placeholder='password' onChange={this.handleChange('password')} />
-                    <ul>
-                        <br />
-                        {errors}
-                        <br />
-                    </ul>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className='sign-in-continue'>
-
-                            <button type='submit'>Sign In</button>
-                        </div>
-                    </form>
-
+    return (
+        <div className='sign-in-form'>
+            <div className='sign-in-title'>
+                <Link to='/'><img src={window.big} /></Link>
+            </div>
+            <div className='sign-in-body'>
+                <h1>Sign In</h1>
+                <input type="text" value={email} placeholder='email' onChange={handleChangeEmail} />
+                <input type="password" value={password} placeholder='password' onChange={handleChangePassword} />
+                <ul>
                     <br />
-                    <div className='redirect-to-sign-up'>
-                        <h3>New to PatrickFlix?</h3>
-                        <Link to='/signup'> Sign up now.</Link>
+                    {props.errors  ? props.errors.map((error, idx) => {
+                        return <li key={idx}>{error}</li>
+                    }) : ''}
+                    <br />
+                </ul>
+                <form onSubmit={handleSubmit}>
+                    <div className='sign-in-continue'>
+
+                        <button type='submit'>Sign In</button>
                     </div>
+                </form>
+
+                <br />
+                <div className='redirect-to-sign-up'>
+                    <h3>New to PatrickFlix?</h3>
+                    <Link to='/signup'> Sign up now.</Link>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+    
 }
 
 export default LogInForm;
