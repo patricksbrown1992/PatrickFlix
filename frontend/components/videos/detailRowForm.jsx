3b06e0d7512a60ad8390bdf714c53b5b5f9a7e75
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-26 01:25:02 -0400

    convert detail row form to hook

--- frontend/components/videos/detailRowForm.jsx
+++ frontend/components/videos/detailRowForm.jsx
@@ -1,118 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import IndexItem from './indexItemContainer';
-import HiddenRowContainer from './hiddenRowContainer';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import IndexItem from "./indexItemContainer";
+import HiddenRowContainer from "./hiddenRowContainer";
 
+const DetailRowForm = (props) => {
+  const [start, updateStart] = useState(() => {
+    return 0;
+  });
 
-class DetailRowForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { open: false, start: 0 };
-        this.right = this.right.bind(this);
-        this.left = this.left.bind(this);
-        
+  const [selectedVideo, updateSelectedVideo] = useState(() => {
+    return null;
+  });
 
+  function right(e) {
+    e.preventDefault();
+    if (start < props.vids.length - 4) {
+      updateStart(start + 1);
     }
+  }
 
-  
-
-
-    right(){
-        
-        if(this.state.start < this.props.vids.length-4){
-            this.setState({start: this.state.start += 1})
-        }
-        
+  function left(e) {
+    e.preventDefault();
+    if (start > 0) {
+      updateStart(start - 1);
     }
+  }
 
-    left(){
-        if(this.state.start > 0){
-            this.setState({start: this.state.start -= 1})
-        }
-        
+  let vids = props.vids.map((vid, index) => (
+    <IndexItem
+      selectedVideo={selectedVideo}
+      updateSelectedVideo={updateSelectedVideo}
+      key={vid.id}
+      video={vid}
+    />
+  ));
 
-    }
+  vids = vids.slice(start, start + 4);
 
+  const hiddenRowDiv =
+    selectedVideo !== null ? (
+      <HiddenRowContainer vid={props.videos[selectedVideo]} />
+    ) : (
+      ""
+    );
 
-   
-    render() {
-        let vid;
-        
-        let video_row = document.getElementById(`video-row-inner-${this.props.identifier}`);
-        
-        let hidden_row_div;
-        let left_arrow;
-        let right_arrow;
-        
-        let vids = this.props.vids.map((vid, index) => (
-            
-            <IndexItem  key ={index + this.props.range[0]} video={vid} index={index + this.props.range[0]}/>
-        ))
-        
-        vids = vids.slice(this.state.start,this.state.start + 4)
-        
-        if(this.props.modal !== null &&  this.props.range.includes(this.props.modal)){
+  const toggleVideoRow =
+    selectedVideo === null ? "video-row-inner-hide" : "video-row-inner-show";
 
-            vid = this.props.videos[this.props.modal]
-            hidden_row_div = <HiddenRowContainer vid={vid}/>
-            
-            video_row.style.height = '600px';
-            
-         
-            
-        } 
-        else {
-     
-  
-            hidden_row_div = ''
-        
-         
-            
-        
-            if(video_row){
-                video_row.style.height = '200px';
-            }
-   
-        }
+  const leftArrow = start ? (
+    <div onClick={left} className="left-arrow-div">
+      <i className="fas fa-chevron-left fa-3x"></i>
+    </div>
+  ) : (
+    ""
+  );
+  const rightArrow =
+    start < props.vids.length - 4 ? (
+      <div onClick={right} className="right-arrow-div">
+        <i className="fas fa-chevron-right fa-3x"></i>
+      </div>
+    ) : (
+      ""
+    );
 
-        if(this.state.start == 0){
-            left_arrow = '';
-        } else {
-            left_arrow = 
-            <div onClick={this.left} className='left-arrow-div'>
-                <i className="fas fa-chevron-left fa-3x"></i>
-            </div>
-        }
-        
-        if(this.state.start < this.props.vids.length-4){
-            right_arrow = 
-            <div onClick={this.right} className='right-arrow-div'>
-                <i  className="fas fa-chevron-right fa-3x"></i>
-            </div>
-            
-        } else {
-            right_arrow = '';
-        }
-      
-        
-    
-        return (
-            <div key={this.props.identifier} className = "video-row-inner" id = {`video-row-inner-${this.props.identifier}`}>
-                
-                <div className = "top-row">
-                    {left_arrow}
-                    <div className= 'links-row'>
-                        {vids}
-                    </div>
-                    {right_arrow}
-                </div>
-                {hidden_row_div}
-              
-                
-                
-            </div>
-        )
-    }
-
-}
+  return (
+    <div key={props.identifier} className={toggleVideoRow}>
+      <div className="top-row">
+        {leftArrow}
+        <div className="links-row">{vids}</div>
+        {rightArrow}
+      </div>
+      {hiddenRowDiv}
+    </div>
+  );
+};
 export default DetailRowForm;
