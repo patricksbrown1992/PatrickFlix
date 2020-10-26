9b9a886e2d78669c7fae1f94f9501b056b879531
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-10-26 01:22:55 -0400

    convert my list row form to hook

--- frontend/components/videos/myListRowForm.jsx
+++ frontend/components/videos/myListRowForm.jsx
@@ -1,89 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import ListItemContainer from './myListItemContainer';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ListItemContainer from "./myListItemContainer";
 
+const MyListForm = (props) => {
+  const [start, updateStart] = useState(() => {
+    return 0;
+  });
 
-
-class MyListForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { open: false, start: 0 };
-        this.right = this.right.bind(this);
-        this.left = this.left.bind(this);
-        
-
+  function right(e) {
+    e.preventDefault();
+    if (start < props.lists.length - 4) {
+      updateStart(start + 1);
     }
+  }
 
-  
-
-
-    right(){
-        
-        if(this.state.start < this.props.vids.length-4){
-            this.setState({start: this.state.start += 1})
-        }
-        
+  function left(e) {
+    e.preventDefault();
+    if (start > 0) {
+      updateStart(start - 1);
     }
+  }
 
-    left(){
-        if(this.state.start > 0){
-            this.setState({start: this.state.start -= 1})
-        }
-        
+  const left_arrow =
+    start !== 0 ? (
+      <div onClick={left} className="left-arrow-div">
+        <i className="fas fa-chevron-left fa-3x"></i>
+      </div>
+    ) : (
+      ""
+    );
+  const right_arrow =
+    start < props.lists.length - 4 ? (
+      <div onClick={right} className="right-arrow-div">
+        <i className="fas fa-chevron-right fa-3x"></i>
+      </div>
+    ) : (
+      ""
+    );
 
-    }
+  let lists = props.lists.map((list, index) => (
+    <ListItemContainer key={index} list={list} />
+  ));
 
+  lists = lists.slice(start, start + 4);
 
-   
-    render() {
-        let vid;
-        
-        
-        let left_arrow;
-        let right_arrow;
-        
-        let vids = this.props.vids.map((vid, index) => (
-            
-            <ListItemContainer  key ={index} video={vid} />
-        ))
-        
-        vids = vids.slice(this.state.start, this.state.start + 4)
-       
-
-        if(this.state.start == 0){
-            left_arrow = '';
-        } else {
-            left_arrow = 
-            <div onClick={this.left} className='left-arrow-div'>
-                <i className="fas fa-chevron-left fa-3x"></i>
-            </div>
-        }
-        
-        if(this.state.start < this.props.vids.length-4){
-            right_arrow = 
-            <div onClick={this.right} className='right-arrow-div'>
-                <i  className="fas fa-chevron-right fa-3x"></i>
-            </div>
-            
-        } else {
-            right_arrow = '';
-        }
-      
-        
-     
-   
-        return (
-            <div className = "my-lists-row">
-                {left_arrow}
-                <div className= 'lists-links-row'>
-                    {vids}
-                </div>
-                {right_arrow}
-            </div>
- 
-   
-        )
-    }
-
-}
+  return (
+    <div className="my-lists-row">
+      {left_arrow}
+      <div className="lists-links-row">{lists}</div>
+      {right_arrow}
+    </div>
+  );
+};
 export default MyListForm;
